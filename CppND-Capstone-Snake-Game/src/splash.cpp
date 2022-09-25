d02f9694f054425ce820d00b08e7dbdf77685886
rketch <34118881+rketch@users.noreply.github.com>
2022-09-25 12:03:48 -0400

    Add source code

---
+++ CppND-Capstone-Snake-Game/src/splash.cpp
@@ -0,0 +1,5 @@
+#include "splash.h"
+
+void Splash::Display(std::string filepath){
+  
+}
---
+++ CppND-Capstone-Snake-Game/src/splash.h
@@ -0,0 +1,15 @@
+#ifndef SPLASH_H
+#define SPLASH_H
+
+
+#include <string>
+
+class Splash {
+  /* */
+  public:
+  	void Display(std::string filepath);
+  private:
+  	
+};
+
+#endif
